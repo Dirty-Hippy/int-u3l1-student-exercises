18f136d2825aa7894f7f0f27ddbcb9fc658396a3
Dirty-Hippy <stephanie.s.panayides@gmail.com>
2026-06-17 09:25:02 -0700

    First java script with the fairy tale

--- variables.js
+++ variables.js
@@ -2,17 +2,25 @@
 let favoriteColor;
 
 // 1. Create a variable called `favoriteFood`.
+let favoriteFood;
 
 
 // 2. Create a variable called `favoriteNumber`.
+let favoriteNumber;
 
 
 
 
 // CODE ALONG, PART 2
 //  3. Assign values for each variables.
+favoriteColor = "purple";
+favoriteFood = "clamChowder";
+favoriteNumber = 231;
 // - Use a string value of your favorite color.
 // - Use a string value of your favorite food.
 // - Use a number value of your favorite number.
 
 // 4. Console log each variable on a new line.
+console.log(favoriteColor);
+console.log(favoriteFood);
+console.log(favoriteNumber);
